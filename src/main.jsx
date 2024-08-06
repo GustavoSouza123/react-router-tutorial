b66d8f3f2daedd8c52fa93f3cf5c7d06bf0de65c
Gustavo Souza <gustavoelia7@gmail.com>
2024-08-06 12:17:28 -0300

    create contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
-import Root, { loader as rootLoader } from './routes/root';
+import Root, {
+    loader as rootLoader,
+    action as rootAction,
+} from './routes/root';
+import Contact, { loader as contactLoader } from './routes/contact';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
 
 const router = createBrowserRouter([
     {
@@ -12,16 +15,18 @@ const router = createBrowserRouter([
         element: <Root />,
         errorElement: <ErrorPage />,
         loader: rootLoader,
+        action: rootAction,
         children: [
             {
                 path: 'contacts/:contactId',
                 element: <Contact />,
+                loader: contactLoader,
             },
         ],
     },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router} />
     </React.StrictMode>
