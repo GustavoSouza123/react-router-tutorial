e55d5f65a21371f825c30ff307a76dede1f17dd6
Gustavo Souza <gustavoelia7@gmail.com>
2024-08-06 10:58:46 -0300

    contact route

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,9 +1,19 @@
+import { Outlet, Link, useLoaderData } from 'react-router-dom';
+import { getContacts } from '../contacts';
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
+
 export default function Root() {
+    const { contacts } = useLoaderData();
+
     return (
         <>
             <div id="sidebar">
                 <h1>React Router Contacts</h1>
-                
+
                 <div>
                     <form id="search-form" role="search">
                         <input
@@ -25,19 +35,36 @@ export default function Root() {
                         <button type="submit">New</button>
                     </form>
                 </div>
+
                 <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Your Name</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
-                        </li>
-                    </ul>
+                    {contacts.length ? (
+                        <ul>
+                            {contacts.map((contact) => (
+                                <li key={contact.id}>
+                                    <Link to={`contacts/${contact.id}`}>
+                                        {contact.first || contact.last ? (
+                                            <>
+                                                {contact.first} {contact.last}
+                                            </>
+                                        ) : (
+                                            <i>No name</i>
+                                        )}{' '}
+                                        {contact.favorite && <span>★</span>}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>
+                            <i>No contacts</i>
+                        </p>
+                    )}
                 </nav>
             </div>
 
-            <div id="detail"></div>
+            <div id="detail">
+                <Outlet />
+            </div>
         </>
     );
 }
