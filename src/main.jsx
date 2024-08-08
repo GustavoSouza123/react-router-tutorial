8355fcc05af6a72ab826e9f1e8b641caf74e3d50
Gustavo Souza <gustavoelia7@gmail.com>
2024-08-08 10:50:50 -0300

    index route

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Contact, { loader as contactLoader } from './routes/contact';
 import ErrorPage from './error-page';
 import EditContact, { action as editAction } from './routes/edit';
 import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
 const router = createBrowserRouter([
     {
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
         loader: rootLoader,
         action: rootAction,
         children: [
+            {
+                index: true,
+                element: <Index />
+            },
             {
                 path: 'contacts/:contactId',
                 element: <Contact />,
@@ -33,7 +38,7 @@ const router = createBrowserRouter([
             {
                 path: 'contacts/:contactId/destroy',
                 action: destroyAction,
-                errorElement: <div>Oops! There was an error.</div>
+                errorElement: <div>Oops! There was an error.</div>,
             },
         ],
     },
--- src/routes/destroy.jsx
+++ src/routes/destroy.jsx
@@ -1,8 +1,7 @@
-import { redirect } from "react-router-dom";
-import { deleteContact } from "../contacts";
+import { redirect } from 'react-router-dom';
+import { deleteContact } from '../contacts';
 
 export async function action({ params }) {
-    throw new Error('something bad happened');
     await deleteContact(params.contactId);
     return redirect('/');
 }
